3ac4c3d9bd3e448acbbd568ed3f87054b826ff6e
ralf@ackstorm.de <ralf@ackstorm.de>
2014-05-04 17:06:55 +0200

    Prevent onItemSelectedHandler on save/restore
    The onItemSelectedListener gets called asynchronously after
    saved state has been applied, resulting in the unit spinner to be
    reset to default.
    
    There seems to be no way to prevent the handler from running for non-user
    initiated state change of the spinner.
    
    Work around this by only changing the unit spinner if the current pos
    is different from what has been set from saved state. This effectively
    disabled the handler as long as the selected category is not changed.

--- src/de/ackstorm/converter/MainActivity.java
+++ src/de/ackstorm/converter/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends Activity {
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "MainActivity.onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
@@ -73,28 +72,25 @@ public class MainActivity extends Activity {
     	protected Spinner mUnitSpinner;
     	protected Spinner mCategorySpinner;
     	protected EditText mEditText;
-    	/* protected TextView mOutputText; */
     	protected ListView mOutputList;
+    	protected Integer mSavedCategory = null;
     	
-    	private static final String KEY_TEXT = "text";
     	private static final String KEY_CATEGORY = "category";
     	private static final String KEY_UNIT = "unit";
     	
         @Override
         public void onSaveInstanceState(Bundle outState) {
             super.onSaveInstanceState(outState);
-            /* outState.putString(KEY_TEXT, mOutputText.getText().toString()); */
-            outState.putInt(KEY_CATEGORY, mCategorySpinner.getSelectedItemPosition());
-            outState.putInt(KEY_UNIT, mUnitSpinner.getSelectedItemPosition());
-            Log.d(TAG, "onSaveInstanceState cat=" + mCategorySpinner.getSelectedItemPosition());
-            Log.d(TAG, "onSaveInstanceState uni=" + mUnitSpinner.getSelectedItemPosition());
+            outState.putInt(KEY_CATEGORY, Integer.valueOf(mCategorySpinner.getSelectedItemPosition()));
+            outState.putInt(KEY_UNIT, Integer.valueOf(mUnitSpinner.getSelectedItemPosition()));
         }
     	
     	@Override
     	public void onCreate(Bundle savedInstanceState){
     		super.onCreate(savedInstanceState);
     		mUnitConverter = new UnitConverter();
-    	}
+  	}
+    	
     	
         @Override
         public View onCreateView(LayoutInflater inflater,
@@ -102,33 +98,42 @@ public class MainActivity extends Activity {
         						 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
                         
-            // mConvertButton   = (Button)   rootView.findViewById(R.id.convert_button);
 			mUnitSpinner     = (Spinner)  rootView.findViewById(R.id.unit_spinner);
 			mCategorySpinner = (Spinner)  rootView.findViewById(R.id.category_spinner);
 	    	mEditText        = (EditText) rootView.findViewById(R.id.edit_message);
-	    	/* mOutputText      = (TextView) rootView.findViewById(R.id.output_text); */
 	    	mOutputList      = (ListView) rootView.findViewById(R.id.output_list);
 
-            Log.d(TAG, "onCreateView");
         	if (savedInstanceState != null)	{
-                Log.d(TAG, "onCreateView: text="+ savedInstanceState.getString(KEY_TEXT));
-                Log.d(TAG, "onCreateView: cat="+ savedInstanceState.getInt(KEY_CATEGORY));
-                Log.d(TAG, "onCreateView: uni="+ savedInstanceState.getInt(KEY_UNIT));
-        		
-        		/* mOutputText.setText(savedInstanceState.getString(KEY_TEXT)); */
+        		int unit = savedInstanceState.getInt(KEY_UNIT);
+        		int category = savedInstanceState.getInt(KEY_CATEGORY);
+
+        		mSavedCategory = category;
             	populateCategorySpinner();
-            	// TODO this doesn't seem to work
-            	populateUnitSpinner(savedInstanceState.getInt(KEY_CATEGORY));
-            	mUnitSpinner.setSelection(savedInstanceState.getInt(KEY_UNIT));
+            	mCategorySpinner.setSelection(category);
+            	populateUnitSpinner(category);
+            	mUnitSpinner.setSelection(unit);
+        	} else {
+            	populateCategorySpinner();
+            	populateUnitSpinner(0);
         	}
-        	populateCategorySpinner();
 
         	mCategorySpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
         		
         		@Override
                 public void onItemSelected(AdapterView<?> parent, View view, 
                         int category, long id) {
-            		populateUnitSpinner(category);
+            		/*
+            		 * The onItemSelectedListener gets called after saved state has been
+            		 * applied, resulting in the unit spinner to be reset to default.
+            		 * Prevent this by only changing the unit spinner if the current pos
+            		 * is different from what has been set from saved state.
+            		 */
+                    if (mSavedCategory != null) {
+                    	if (mSavedCategory != category) {
+                        	populateUnitSpinner(category);
+                        	mSavedCategory = null;
+                    	}
+        			}
             		updateResult();
                 }
                 
@@ -154,34 +159,20 @@ public class MainActivity extends Activity {
         		
         	});
 
-//	    	mConvertButton.setOnClickListener(new View.OnClickListener() {
-//				
-//				@Override
-//				public void onClick(View view) {
-//					updateResult();
-//				}
-//				
-//			});
-
         	mEditText.addTextChangedListener(new TextWatcher() {
         		
 				@Override
 				public void beforeTextChanged(CharSequence s, int start,
 						int count, int after) {
-					// TODO Auto-generated method stub
-					
 				}
 
 				@Override
 				public void onTextChanged(CharSequence s, int start,
 						int before, int count) {
-					// TODO Auto-generated method stub
-					
 				}
 
 				@Override
 				public void afterTextChanged(Editable s) {
-					// TODO Auto-generated method stub
 					updateResult();
 				}
 	    		
@@ -202,7 +193,6 @@ public class MainActivity extends Activity {
         	ArrayList<Integer> categoryIds;
            	ArrayList<CharSequence> categoryStrings;
 
-        	Log.d(TAG, "populateCategorySpinner");
            	categoryIds = mUnitConverter.getCategories();
         	categoryStrings = new ArrayList<CharSequence>();
 
@@ -216,7 +206,6 @@ public class MainActivity extends Activity {
         	ArrayList<Unit> units;
            	ArrayList<CharSequence> unitStrings;
 
-        	Log.d(TAG, "populateUnitSpinner(" + category + ")");
            	units = mUnitConverter.getUnits(category);
         	unitStrings = new ArrayList<CharSequence>();
 
@@ -234,49 +223,38 @@ public class MainActivity extends Activity {
         }
 
         public void updateResult() {
-			ArrayList<UnitValue> result = new ArrayList<UnitValue>();
-			double value;
+			ResultAdapter adapter;
+			ArrayList<UnitValue> result;
 			String valueString;
-			int unitIndex = mUnitSpinner.getSelectedItemPosition();
-			int categoryIndex = mCategorySpinner.getSelectedItemPosition();
+			double value;
+			int unitIndex;
+			int categoryIndex;
 
-			Log.d(TAG, "updateResult unitIndex=" + unitIndex + " categoryIndex=" + categoryIndex);
+			result = new ArrayList<UnitValue>();
+			unitIndex = mUnitSpinner.getSelectedItemPosition();
+			categoryIndex = mCategorySpinner.getSelectedItemPosition();
 			
 			if (unitIndex < 0 || categoryIndex < 0) {
-				// might happen when TextEdit is modified
-				// before Spinners are populated (seen when
-				// switching between landscape and portrait
-				/* mOutputText.setText(""); */
-	            Log.d(TAG, "updateResult: invalid index");
+				adapter = new ResultAdapter(result);
+				mOutputList.setAdapter(adapter);
 	            return;
 			}
 			
 			valueString = mEditText.getText().toString().trim();
 
 			if (valueString.isEmpty()) {
-				/* mOutputText.setText(""); */
-	            Log.d(TAG, "updateResult: empty valueString");
-			} else { 
-				try {
-					value = Float.valueOf(valueString.trim()).floatValue();
-					result = mUnitConverter.convert(categoryIndex, unitIndex, value);
-				/*
-				for (int k: result.keySet()) {
-					String unit = getActivity().getApplicationContext().getString(k);
-					output += result.get(k).toString() + " " + unit + "\n";
-				}
-				*/
-				} catch (NumberFormatException e) {
-					Log.d(TAG, "updateResult: invalid number format");
-				}
+				adapter = new ResultAdapter(result);
+				mOutputList.setAdapter(adapter);
+				return;
 			}
-			/*
-			ArrayAdapter<UnitValue> adapter =
-					new ArrayAdapter<UnitValue>(getActivity(),
-							android.R.layout.simple_list_item_1,
-							result);
-			*/
-			ResultAdapter adapter = new ResultAdapter(result);
+	            
+			try {
+				value = Float.valueOf(valueString.trim()).floatValue();
+				result = mUnitConverter.convert(categoryIndex, unitIndex, value);
+			} catch (NumberFormatException e) {
+				Log.d(TAG, "updateResult: invalid number format");
+			}
+			adapter = new ResultAdapter(result);
 			mOutputList.setAdapter(adapter);
         }
         
